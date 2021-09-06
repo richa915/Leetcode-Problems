02ef89f5e18a1851de4dc6ac12fcde15cb316eae
richa915 <83656432+richa915@users.noreply.github.com>
2021-09-07 01:08:39 +0530

    Create Roman to Integer.cpp

---
+++ Strings/Roman to Integer.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+        int sum = 0;
+        for(int i=0; i<s.size(); i++){
+            
+            if(s[i] == 'I'){
+                if(i<s.size()-1){
+                    if(s[i+1] == 'V') sum+=4, i+=1;
+                    else if(s[i+1] == 'X') sum+=9, i+=1;
+                    else sum+=1;
+                }
+                else{
+                    sum+=1;
+                }
+            }
+            
+            else if(s[i] == 'X'){
+                if(i<s.size()-1){
+                    if(s[i+1] == 'L') sum+=40, i+=1;
+                    else if(s[i+1] == 'C') sum+=90, i+=1;
+                    else sum+=10;
+                }
+                else{
+                    sum+=10;
+                }
+            }
+            
+            else if(s[i] == 'C'){
+                if(i<s.size()-1){
+                    if(s[i+1] == 'D') sum+=400, i+=1;
+                    else if(s[i+1] == 'M') sum+=900, i+=1;
+                    else sum+=100;
+                }
+                else{
+                    sum+=100;
+                }                
+            }
+            
+            else if(s[i] == 'V') sum+=5;
+            else if(s[i] == 'L') sum+=50;
+            else if(s[i] == 'D') sum+=500;
+            else if(s[i] == 'M') sum+=1000;
+            //cout<<sum<<" ";
+        }
+        
+        return sum;
+    }
+};
